d65844fbe35f23f83036d365d2786272af3c72c4
Yuriy Sorokin <ysorokin.contractor@libertyglobal.com>
2022-09-27 23:08:07 +0600

    Update package version obtaining.
    Signed-off-by: Yuriy Sorokin <ysorokin.contractor@libertyglobal.com>

--- rss_reader/_version.py
+++ rss_reader/_version.py
@@ -1,7 +1,2 @@
-# try:
-#     from importlib import metadata
-# except ImportError: # for Python<3.8
-#     import importlib_metadata as metadata
-# __version__ = metadata.version("time")
-# print(__version__)
-__version__ = "0.0.1"
+from importlib import metadata
+__version__ = metadata.version("rss_reader")
